d5f0608819b73f962c0b395f625cc686867e4afe
Haddouche Maamar <frocodeof@gmail.com>
2023-05-11 14:58:11 +0200

    Change Buttons styles and fixing Js errors

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
 const questions =[
     {
-        question: "Why must the script.js file go at the bottom of the html?"
+        question: "Why must the script.js file go at the bottom of the html?",
         answers: [
-        { text: "because it won't work otherwise", correct: false},
-        { text: "it would cause compatibility issues if not", correct: false}
-        { text: "it helps the page load faster", correct: true}
-        { text: "it helps different devices recognize it better", correct: false}
+            { text: "because it won't work otherwise", correct: false},
+            { text: "it would cause compatibility issues if not", correct: false},
+            { text: "it helps the page load faster", correct: true},
+            { text: "it helps different devices recognize it better", correct: false}
         ]
-        
+
     },
     {
-        question: "The condition in an if/ else statement is enclosed with _________"
+        question: "The condition in an if/ else statement is enclosed with _________",
         answers: [
-        { text: "quotes", correct: false},
-        { text: "curly brackets", correct: false}
-        { text: "parenthesis", correct: true}
-        { text: "square brackets", correct: false}
-        ]
-    },
-    {
-        question: "A very useful tool for publicly storing code"
-        answers: [
-        { text: "Github/Bash", correct: true},
-        { text: "Git", correct: false}
-        { text: "Devconsole", correct: false}
-        { text: "intellij", correct: false}
+            { text: "quotes", correct: false},
+            { text: "curly brackets", correct: false},
+            { text: "parenthesis", correct: true},
+            { text: "square brackets", correct: false}
         ]
     },
     {
-        question: "What is a Variable?"
+        question: "A very useful tool for publicly storing code",
         answers: [
-        { text: "a string", correct: false},
-        { text: "a value", correct: true}
-        { text: "a boolean", correct: false}
-        { text: "a stored loop that can change depending on the function and parameters", correct: false}
+            { text: "Github/Bash", correct: true},
+            { text: "Git", correct: false},
+            { text: "Devconsole", correct: false},
+            { text: "intellij", correct: false}
         ]
     },
     {
-        question: "what does DOM stand for?"
+        question: "What is a Variable?",
         answers: [
-        { text: "Document Operation Manipulator", correct: false},
-        { text: "Developer Operation Model", correct: false}
-        { text: "Document Object Mover", correct: false}
-        { text: "Document Object Model", correct: true}
+            { text: "a string", correct: false},
+            { text: "a value", correct: true},
+            { text: "a boolean", correct: false},
+            { text: "a stored loop that can change depending on the function and parameters", correct: false}
+        ]
+    },
+    {
+        question: "what does DOM stand for?",
+        answers: [
+            { text: "Document Operation Manipulator", correct: false},
+            { text: "Developer Operation Model", correct: false},
+            { text: "Document Object Mover", correct: false},
+            { text: "Document Object Model", correct: true}
         ]
     }
 ];
